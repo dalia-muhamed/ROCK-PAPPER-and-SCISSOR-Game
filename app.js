e2f21611f24936ab01721a1a63e44ff974b6e940
dalia.muhammed <dalia.muhammed1525@gmail.com>
2023-03-02 22:15:13 +0200

    adding computer choice function

--- app.js
+++ app.js
@@ -5,23 +5,50 @@ const PAPPER = 'PAPPER';
 const SCISSOR = 'SCISSOR';
 const defaultValue = ROCK;
 let gameRunning = false;
+const draw = "it's Draw";
+const playerWin = 'Player Wins';
+const computerWin = 'Computer Wins';
 
 const getPlayerChoise = function () {
-  const selection = prompt(
-    `Choose ${ROCK} ,${PAPPER} or ${SCISSOR}`
-  ).toUpperCase();
+  const selection = prompt(` ${ROCK} ,${PAPPER} or ${SCISSOR} ?`).toUpperCase();
   if (selection !== ROCK && selection !== PAPPER && selection !== SCISSOR) {
     alert(`You enter an Invalid choise, I choose ${defaultValue} for you`);
     return defaultValue;
   }
   return selection;
 };
-const startGame = startGameBtn.addEventListener('click', function start() {
+const getComputerChoice = function () {
+  const randomNumber = Math.random();
+  if (randomNumber < 0.34) {
+    return ROCK;
+  } else if (randomNumber < 0.67) {
+    return PAPPER;
+  } else {
+    return SCISSOR;
+  }
+};
+
+const getWinner = function (cChoice, pChoice) {
+  if (cChoice === pChoice) {
+    return draw;
+  } else if (
+    (cChoice === ROCK && pChoice === PAPPER) ||
+    (cChoice === PAPPER && pChoice === SCISSOR) ||
+    (cChoice === SCISSOR && pChoice === ROCK)
+  ) {
+    return playerWin;
+  } else {
+    return computerWin;
+  }
+};
+startGameBtn.addEventListener('click', function start() {
   if (gameRunning) {
     return;
   }
   gameRunning = true;
   console.log('Game is starting...');
-  const playerSelection = getPlayerChoise();
-  console.log(playerSelection);
+  const playerChoice = getPlayerChoise();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  console.log(winner);
 });

59a70793936e3d0e775d94531fa0fb9772ef40d7
dalia.muhammed <dalia.muhammed1525@gmail.com>
2023-03-03 00:52:51 +0200

    shorten some code

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const ROCK = 'ROCK';
 const PAPPER = 'PAPPER';
 const SCISSOR = 'SCISSOR';
 const defaultValue = ROCK;
-let gameRunning = false;
 const draw = "it's Draw";
-const playerWin = 'Player Wins';
-const computerWin = 'Computer Wins';
+const playerWin = 'You Win';
+const computerWin = 'Computer Win';
 
-const getPlayerChoise = function () {
+const getPlayerChoise = () => {
   const selection = prompt(` ${ROCK} ,${PAPPER} or ${SCISSOR} ?`).toUpperCase();
   if (selection !== ROCK && selection !== PAPPER && selection !== SCISSOR) {
     alert(`You enter an Invalid choise, I choose ${defaultValue} for you`);
@@ -17,38 +16,53 @@ const getPlayerChoise = function () {
   }
   return selection;
 };
-const getComputerChoice = function () {
+const getComputerChoice = () => {
   const randomNumber = Math.random();
-  if (randomNumber < 0.34) {
-    return ROCK;
-  } else if (randomNumber < 0.67) {
-    return PAPPER;
-  } else {
-    return SCISSOR;
-  }
+  return randomNumber < 0.34 ? ROCK : randomNumber < 0.67 ? PAPPER : SCISSOR;
+  // if (randomNumber < 0.34) {
+  //   return ROCK;
+  // } else if (randomNumber < 0.67) {
+  //   return PAPPER;
+  // } else {
+  //   return SCISSOR;
+  // }
 };
 
-const getWinner = function (cChoice, pChoice) {
-  if (cChoice === pChoice) {
-    return draw;
-  } else if (
-    (cChoice === ROCK && pChoice === PAPPER) ||
-    (cChoice === PAPPER && pChoice === SCISSOR) ||
-    (cChoice === SCISSOR && pChoice === ROCK)
-  ) {
-    return playerWin;
-  } else {
-    return computerWin;
-  }
-};
-startGameBtn.addEventListener('click', function start() {
-  if (gameRunning) {
-    return;
-  }
-  gameRunning = true;
+const getWinner = (cChoice, pChoice) =>
+  cChoice === pChoice
+    ? draw
+    : (cChoice === ROCK && pChoice === PAPPER) ||
+      (cChoice === PAPPER && pChoice === SCISSOR) ||
+      (cChoice === SCISSOR && pChoice === ROCK)
+    ? playerWin
+    : computerWin;
+//arrow function if you use only one exp you can remove curly braces & in this case you have to remove return keyword in front of exp it already written by default in front of exp
+
+// if (cChoice === pChoice) {
+//   return draw;
+// } else if (
+//   (cChoice === ROCK && pChoice === PAPPER) ||
+//   (cChoice === PAPPER && pChoice === SCISSOR) ||
+//   (cChoice === SCISSOR && pChoice === ROCK)
+// ) {
+//   return playerWin;
+// } else {
+//   return computerWin;
+// }
+
+startGameBtn.addEventListener('click', () => {
   console.log('Game is starting...');
   const playerChoice = getPlayerChoise();
   const computerChoice = getComputerChoice();
   const winner = getWinner(computerChoice, playerChoice);
   console.log(winner);
+  let message = `You picked ${playerChoice} and Computer picked ${computerChoice} so `;
+  if (winner === draw) {
+    message += `${draw}`;
+  } else if (winner === playerWin) {
+    message += `${playerWin}`;
+  } else {
+    message += `${computerWin}`;
+  }
+  alert(message);
 });
